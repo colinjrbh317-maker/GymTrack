afbb22e295315e902ec2d168f834d5efc8074d9f
Colin <colinjrbh317@icloud.com>
2025-09-16 12:49:02 -0400

    Initial Commit

---
+++ GymTrackTests/GymTrackTests.swift
@@ -0,0 +1,16 @@
+//
+//  GymTrackTests.swift
+//  GymTrackTests
+//
+//  Created by Colin Ryan on 9/16/25.
+//
+
+import Testing
+
+struct GymTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
